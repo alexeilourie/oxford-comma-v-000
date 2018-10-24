de2e5852d851275bf8c9b96155d50c745b0e889b
Alexei Lourie <alexeilourie@gmail.com>
2018-10-24 00:26:02 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,6 +3,8 @@ def oxford_comma(array)
   array.each do |word|
     if word == array[-1]
       str << ", and #{word}"
+    elsif word == array[0]
+      str << "#{word}"
     else
       str << ", #{word}"
     end
